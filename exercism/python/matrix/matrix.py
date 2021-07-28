efbfd5292e0a1e843ec66e0b2ddfd96ccca37794
Austin <alaturno@gmail.com>
2021-07-27 20:10:52 -0700

    python matrix solution

--- exercism/python/matrix/matrix.py
+++ exercism/python/matrix/matrix.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python3 
+
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        self.matrix = list(matrix_string.splitlines())
 
     def row(self, index):
-        pass
+        # Creating list from row index, then converting to integers.
+        r = list(self.matrix[index-1].split())
+        return list(map(lambda x: int(x), r))
+        
 
     def column(self, index):
-        pass
+        # Creating list of lists, pulling column index from each into
+        # a new list, then converting to integers.
+        c = list(i.split() for i in self.matrix)
+        c = list(i[index-1]for i in c)
+        return list(map(lambda x: int(x), c))

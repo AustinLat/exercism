0664d9bc4f392856359528e3a942f29c48e53ba9
AustinLat <alaturno@gmail.com>
2021-07-29 19:31:27 -0700

    Added type hint

--- exercism/python/hamming/hamming.py
+++ exercism/python/hamming/hamming.py
@@ -1,2 +1,11 @@
-def distance(strand_a, strand_b):
-    pass
+#!/usr/bin/env python3
+
+def distance(strand_a: str, strand_b: str) -> int:
+    """Take two strands as strings, and return the hamiming distance."""
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Input must be valid strands of equal length.")
+    ham = 0
+    for i in range(len(strand_a)):
+        if strand_a[i] != strand_b[i]:
+            ham += 1
+    return ham

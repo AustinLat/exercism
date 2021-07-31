44da5ac4cfe87246db2f9894192a70a48f05c94f
AustinLat <alaturno@gmail.com>
2021-07-30 21:51:56 -0700

    refactored to make code more Pythonic.  Used list comprehension instread of formal for-loops.

--- exercism/python/hamming/hamming.py
+++ exercism/python/hamming/hamming.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 
 def distance(strand_a: str, strand_b: str) -> int:
-    """Take two strands as strings, and return the hamiming distance."""
+    """Take two strands as strings, and return the hamming distance."""
     if len(strand_a) != len(strand_b):
         raise ValueError("Input must be valid strands of equal length.")
-    ham = 0
-    for i in range(len(strand_a)):
-        if strand_a[i] != strand_b[i]:
-            ham += 1
-    return ham
+    return len(["Not Equal" for a, b in zip(strand_a, strand_b) if a != b])

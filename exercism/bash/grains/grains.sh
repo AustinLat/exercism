3a98df690dcceeb24aeb0daa382323a8f0676920
AustinLat <alaturno@gmail.com>
2021-08-02 18:20:48 -0700

    almost have grains solved.

---
+++ exercism/bash/grains/grains.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -x
+main(){
+  if [[ $1 < 1 ]] || [[ $1 > 64 ]]; then
+    echo "Error: invalid input" && exit 1
+  fi
+#  if [[ $1 == "total" ]]; then
+#    echo "18446744073709551615" && exit
+#  fi
+  total=1
+  i=1
+  while [[ $i -lt $1 ]]; do
+    total=$((total * 2)) 
+    ((i+=1))
+  done 
+  echo "$total"
+}
+
+main "$@"

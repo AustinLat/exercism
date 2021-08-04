bae0e5bbc507f8b9c2512c1735c3729f505249a8
AustinLat <alaturno@gmail.com>
2021-08-03 20:22:33 -0700

    Solved by using bc instead of bash arithmetic operator.  No idea why this is.

--- exercism/bash/grains/grains.sh
+++ exercism/bash/grains/grains.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-#set -x
+# For some reason, using normal bash arithmetic: total=$((total*2)) 
+# would return a negative number when the given input is 64.  
+# Using bc doesn't return a negative number.
+
 main(){
   if [[ $1 == "total" ]]; then
     echo "18446744073709551615" && exit 0
@@ -8,11 +11,10 @@ main(){
   if [[ $1 < 1 ]] || [[ $1 > 64 ]]; then
     echo "Error: invalid input" && exit 1
   fi
-
   total=1
   i=1
   while [[ $i -lt $1 ]]; do
-    total=$((total * 2)) 
+    total=$(echo "${total} * 2" | bc)
     ((i+=1))
   done 
   echo "$total"

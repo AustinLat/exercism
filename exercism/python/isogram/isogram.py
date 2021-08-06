e3412cefab1765fa9b19e631279070ea84b59c5f
AustinLat <alaturno@gmail.com>
2021-08-05 17:05:17 -0700

    isogram solved

--- exercism/python/isogram/isogram.py
+++ exercism/python/isogram/isogram.py
@@ -1,2 +1,10 @@
-def is_isogram(string):
-    pass
+#!/usr/bin/env python3
+
+def is_isogram(string: str) -> bool:
+    """Remove all occurences of whitespace and hyphens from the
+    input string. Convert to lowercase, then create a set and
+    compare set to string lengths."""
+    string=string.replace(" ", "")
+    string=string.replace("-", "").lower()
+    isoset={i for i in string}
+    return len(string) == len(isoset)

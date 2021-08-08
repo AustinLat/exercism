b397cd8c477c24ea7f528f7c9f0da39c016acb59
AustinLat <alaturno@gmail.com>
2021-08-08 16:04:38 -0700

    Luhn algorithm solved.

--- exercism/bash/luhn/luhn.sh
+++ exercism/bash/luhn/luhn.sh
@@ -1,28 +1,43 @@
 #!/usr/bin/env bash
 
 main() {
+  # Rmoving whitespace from input
   num=${1//[[:blank:]]/}
+  
+  # Checking for invalid input.
+  if (( ${#num} < 2 )); then
+    echo "false"; exit
+  fi
+  if ! [[ $num =~ ^[0-9]+$ ]]; then
+    echo "false"; exit
+  fi
+  
+  #reversing number
   revnum=""
   for (( i=${#num}-1; i!=-1; i-- )); do
     revnum+=${num:i:1}
   done
+  
+  # Performing doubling on every other number, and adding to total
   total=0
   for i in $(seq 1 ${#revnum}); do
-    if [[ i%2 -eq 0 ]]; then
+    if (( (i%2) == 0 )); then
       double=$((${revnum:i-1:1}*2))
-      doubsum=0
-      for d in $(seq 1 ${#double}); do
-        doubsum=$((doubsum + ${double:d-1:1}))
-      done
-      total=$((total + doubsum))
+      if (( $double > 9 )); then
+        double=$((double - 9))
+      fi
+      total=$((total + double))
     else
       total=$((total + ${revnum:i-1:1}))
     fi
   done
-  mod=$((total % 10))
-  echo $mod
 
-
+  # Echo whether total is divisible by 10
+  if (( total % 10 == 0 )); then
+    echo "true"
+  else
+    echo "false"
+  fi
 }
 
 main "$@"

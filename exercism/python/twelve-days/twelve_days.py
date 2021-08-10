c50a9c06839daad18e9f64c7a1b50fbf6f7b7477
AustinLat <alaturno@gmail.com>
2021-08-09 19:36:29 -0700

    working on twelve days

--- exercism/python/twelve-days/twelve_days.py
+++ exercism/python/twelve-days/twelve_days.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
 
 def recite(start_verse, end_verse):
-    song = [
-            "a Partridge in a Pear Tree."
-            "two Turtle Doves, "
-            "three French Hens, "
-            "four Calling Birds, "
-            "five Gold Rings, "
-            "six Geese-a-Laying, "
-            "seven Swans-a-Swimming, "
-            "eight Maids-a-Milking, "
-            "nine Ladies Dancing, "
-            "ten Lords-a-Leaping, "
-            "eleven Pipers Piping, "
-            "twelve Drummers Drumming, "
-    ]
-    lines = ""
-    #for i in range(start_verse):
-    #    lines += song[i]
-    print(song)
-
-if __name__ == "__main__":
-    recite(2, 2)
+    days = {"1": ["first", "and a Partridge in a Pear Tree."],
+        "2": ["second", "two Turtle Doves, "],
+        "3": ["third", "three French Hens, "],
+        "4": ["fourth", "four Calling Birds, "],
+        "5": ["fifth", "five Gold Rings, "],
+        "6": ["sixth", "six Geese-a-laying, "],
+        "7": ["seventh", "seven Swans-a-Swimming, "],
+        "8": ["eighth", "eight Maids-a-Milking, "],
+        "9": ["ninth", "nine Ladies Dancing, " ],
+        "10": ["tenth", "ten Lords-a-Leaping, "],
+        "11": ["eleventh", "eleven Pipers Piping, "],
+        "12": ["twelfth", "twleve Drummers Drumming, "]}
+    song = []
+    #print(f"On the {days[str(start_verse)][0]} day of Christmas my true love gave to me: ")
+    for i in range(start_verse, 0, -1):
+        song.append(days[str(i)][1])
+    song.append(f"On the {days[str(start_verse)][0]} day of Christmas my true love gave to me: ")
+    return [i for i in reversed(song)]
+#if __name__ == "__main__":
+#    recite(2, 2)

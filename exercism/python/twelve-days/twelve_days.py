7c32e120cc3bf5238889a81e961b3e99bd59f52d
AustinLat <alaturno@gmail.com>
2021-08-12 17:39:25 -0700

    Fixed 6 failing tests because of stupid ass spelling errors.

--- exercism/python/twelve-days/twelve_days.py
+++ exercism/python/twelve-days/twelve_days.py
@@ -6,13 +6,13 @@ def recite(start_verse, end_verse):
         "3": ["third", "three French Hens, "],
         "4": ["fourth", "four Calling Birds, "],
         "5": ["fifth", "five Gold Rings, "],
-        "6": ["sixth", "six Geese-a-laying, "],
+        "6": ["sixth", "six Geese-a-Laying, "],
         "7": ["seventh", "seven Swans-a-Swimming, "],
         "8": ["eighth", "eight Maids-a-Milking, "],
         "9": ["ninth", "nine Ladies Dancing, " ],
         "10": ["tenth", "ten Lords-a-Leaping, "],
         "11": ["eleventh", "eleven Pipers Piping, "],
-        "12": ["twelfth", "twleve Drummers Drumming, "]}
+        "12": ["twelfth", "twelve Drummers Drumming, "]}
     songlist = []
     song = f"On the {days[str(start_verse)][0]} day of Christmas my true love gave to me: "
     for i in range(start_verse, 0, -1):

b180b1db57ce48766991b4283a6b481c35e68761
AustinLat <alaturno@gmail.com>
2021-08-18 20:35:56 -0700

    need to fix decode to not add spaces.

--- exercism/bash/atbash-cipher/atbash_cipher.sh
+++ exercism/bash/atbash-cipher/atbash_cipher.sh
@@ -2,21 +2,25 @@
 
 
 main() {
-  declare -A alphabet
-  alphabet=([a]=z [b]=y [c]=x [d]=w [e]=v [f]=u
+  declare -A enc
+  declare -A dec
+  enc=([a]=z [b]=y [c]=x [d]=w [e]=v [f]=u
     [g]=t [h]=s [i]=r [j]=q [k]=p [l]=o [m]=n
     [n]=m [o]=l [p]=k [q]=j [r]=i [s]=h [t]=g
     [u]=f [v]=e [w]=d [x]=c [y]=b [z]=a)
-
-    #echo ${alphabet[G]}
+  
+  dec=([z]=a [y]=b [x]=c [w]=d [v]=e [u]=f
+    [t]=g [s]=h [r]=i [q]=j [p]=k [o]=l [n]=m
+    [m]=n [l]=o [k]=p [j]=q [i]=r [h]=s [g]=t
+    [f]=u [e]=v [d]=w [c]=x [b]=y [a]=z)
+ 
     input=${2//[[:blank:]]/}
     input=${input,,}
     input=${input//[^[:alnum:]]/}
     st=""
     for (( i=0; i!="${#input}"; i+=1 )); do
+#need to fix decode to not add spaces, only remove them.
       if [[ ${input:$i:1} =~ [0-9] ]]; then
-        #need to figure out why this if statement isnt working and breaking all
-        #tests
         st+=${input:$i:1}
         continue
       fi
@@ -24,7 +28,9 @@ main() {
         st+=" "
       fi
       if [[ ${1} -eq "encode" ]]; then
-        st+=${alphabet[${input:$i:1}]}
+        st+=${enc[${input:$i:1}]}
+      else
+        st+=${dec[${input:$i:1}]}
       fi
     done 
     echo $st

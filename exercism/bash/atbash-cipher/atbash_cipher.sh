35722609d37c1ee92e42e39249c64e4ae9df3ad5
AustinLat <alaturno@gmail.com>
2021-08-18 19:24:08 -0700

    passed most encode tests. Need to work on decode and digit input.

--- exercism/bash/atbash-cipher/atbash_cipher.sh
+++ exercism/bash/atbash-cipher/atbash_cipher.sh
@@ -3,14 +3,31 @@
 
 main() {
   declare -A alphabet
-  alphabet=([A]=Z [B]=Y [C]=X [D]=W [E]=V [F]=U
-    [G]=T [H]=S [I]=R [J]=Q [K]=P [L]=O [M]=N
-    [N]=M [O]=L [P]=K [Q]=J [R]=I [S]=H [T]=G
-    [U]=F [V]=E [W]=D [X]=C [Y]=B [Z]=A)
+  alphabet=([a]=z [b]=y [c]=x [d]=w [e]=v [f]=u
+    [g]=t [h]=s [i]=r [j]=q [k]=p [l]=o [m]=n
+    [n]=m [o]=l [p]=k [q]=j [r]=i [s]=h [t]=g
+    [u]=f [v]=e [w]=d [x]=c [y]=b [z]=a)
 
-    echo ${alphabet[G]}
- 
-    
+    #echo ${alphabet[G]}
+    input=${2//[[:blank:]]/}
+    input=${input,,}
+    input=${input//[^[:alnum:]]/}
+    st=""
+    for (( i=0; i!="${#input}"; i+=1 )); do
+      if [ ${input:$i:1}[:digit:] ]; then
+        #need to figure out why this if statement isnt working and breaking all
+        #tests
+        st+=${input:$i:1}
+        continue
+      fi
+      if (( ${i} % 5 == 0 )); then
+        st+=" "
+      fi
+      if [[ ${1} -eq "encode" ]]; then
+        st+=${alphabet[${input:$i:1}]}
+      fi
+    done 
+    echo $st
 }
 
 main "$@"

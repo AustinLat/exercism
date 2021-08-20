e6920f1a18ce5bf88aa8aec01fc16c592b9a032f
AustinLat <alaturno@gmail.com>
2021-08-19 19:50:03 -0700

    Refactored. Still need to get decode to not add spaces.

--- exercism/bash/atbash-cipher/atbash_cipher.sh
+++ exercism/bash/atbash-cipher/atbash_cipher.sh
@@ -3,12 +3,12 @@
 
 main() {
   declare -A enc
-  declare -A dec
   enc=([a]=z [b]=y [c]=x [d]=w [e]=v [f]=u
     [g]=t [h]=s [i]=r [j]=q [k]=p [l]=o [m]=n
     [n]=m [o]=l [p]=k [q]=j [r]=i [s]=h [t]=g
     [u]=f [v]=e [w]=d [x]=c [y]=b [z]=a)
-  
+
+  declare -A dec
   dec=([z]=a [y]=b [x]=c [w]=d [v]=e [u]=f
     [t]=g [s]=h [r]=i [q]=j [p]=k [o]=l [n]=m
     [m]=n [l]=o [k]=p [j]=q [i]=r [h]=s [g]=t
@@ -19,19 +19,10 @@ main() {
     input=${input//[^[:alnum:]]/}
     st=""
     for (( i=0; i!="${#input}"; i+=1 )); do
-#need to fix decode to not add spaces, only remove them.
-      if [[ ${input:$i:1} =~ [0-9] ]]; then
-        st+=${input:$i:1}
-        continue
-      fi
-      if (( ${i} % 5 == 0 )); then
-        st+=" "
-      fi
-      if [[ ${1} -eq "encode" ]]; then
-        st+=${enc[${input:$i:1}]}
-      else
-        st+=${dec[${input:$i:1}]}
-      fi
+    #need to fix decode to not add spaces
+      [[ ${input:$i:1} =~ [0-9] ]] && st+=${input:$i:1} && continue
+      [[ ${1} -eq "encode" ]] && (( ${i} % 5 == 0 )) && st+=" "
+      [[ ${1} -eq "encode" ]] && st+=${enc[${input:$i:1}]} || st+=${dec[${input:$i:1}]}
     done 
     echo $st
 }

fd0a1ac03208fcb878485e375bf1f3cfa345f066
AustinLat <alaturno@gmail.com>
2021-08-23 16:21:17 -0700

    adding Python Word Count

---
+++ exercism/python/word-count/word_count.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+def count_words(sentence):
+    dict = {}
+    for word in sentence.split():
+        pass
+
+if __name__ == "__main__":
+    count_words("one fish two fish red fish blue fish")

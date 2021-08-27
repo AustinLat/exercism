da12e89378f3ee35b5f97ef4665b7695d48e9d89
AustinLat <alaturno@gmail.com>
2021-08-27 16:32:11 -0700

    Atbash cipher refactored

--- exercism/bash/atbash-cipher/atbash_cipher.sh
+++ exercism/bash/atbash-cipher/atbash_cipher.sh
@@ -2,24 +2,18 @@
 
 
 main() {
-  declare -A enc
-  enc=([a]=z [b]=y [c]=x [d]=w [e]=v [f]=u
+  declare -A code
+  code=([a]=z [b]=y [c]=x [d]=w [e]=v [f]=u
     [g]=t [h]=s [i]=r [j]=q [k]=p [l]=o [m]=n
     [n]=m [o]=l [p]=k [q]=j [r]=i [s]=h [t]=g
     [u]=f [v]=e [w]=d [x]=c [y]=b [z]=a)
 
-  declare -A dec
-  dec=([z]=a [y]=b [x]=c [w]=d [v]=e [u]=f
-    [t]=g [s]=h [r]=i [q]=j [p]=k [o]=l [n]=m
-    [m]=n [l]=o [k]=p [j]=q [i]=r [h]=s [g]=t
-    [f]=u [e]=v [d]=w [c]=x [b]=y [a]=z)
-  
   declare -l input=${2//[^[:alnum:]]/}
   st=""
   for (( i=0; i!="${#input}"; i+=1 )); do
     [[ ${input:$i:1} =~ [0-9] ]] && st+=${input:$i:1} && continue
     [[ ${1} == "encode" ]] && (( ${i} % 5 == 0 )) && st+=" "
-    [[ ${1} == "encode" ]] && st+=${enc[${input:$i:1}]} || st+=${dec[${input:$i:1}]}
+    st+=${code[${input:$i:1}]}
   done 
   echo $st
 }

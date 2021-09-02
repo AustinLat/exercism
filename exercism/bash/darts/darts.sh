b5afedffea2d9576b18235c4dc4c7e9272c84da3
AustinLat <alaturno@gmail.com>
2021-09-01 17:10:35 -0700

    working on bash darts: Exponents?

---
+++ exercism/bash/darts/darts.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#* If the dart lands outside the target, player earns no points (0 points).
+#* If the dart lands in the outer circle of the target, player earns 1 point.
+#* If the dart lands in the middle circle of the target, player earns 5 points.
+#* If the dart lands in the inner circle of the target, player earns 10 points.
+
+#The outer circle has a radius of 10 units (This is equivalent to the total
+#radius for the entire target), the middle circle a radius of 5 units, and the
+#inner circle a radius of 1.
+
+main() {
+  (( ${#@} != 2 )) && echo "Invalid arguments" && exit 1 
+  echo "arguments valid"
+  (( $1 < 0 )) && lat=$(($1 * -1)) || lat=$1
+  (( $2 < 0 )) && lon=$(($2 * -1)) || lon=$2
+  echo $lat $lon
+  echo $(((e(l(lat)*2)) + (e(l(lon)*2))) | bc -l)
+  echo $distance
+}
+
+main "$@"

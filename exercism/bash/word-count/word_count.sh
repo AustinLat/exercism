bd726de59692d8d74061c3e1a11322af331332ce
AustinLat <alaturno@gmail.com>
2022-01-31 17:20:01 -0800

    Word Count passed all tests

--- exercism/bash/word-count/word_count.sh
+++ exercism/bash/word-count/word_count.sh
@@ -1,16 +1,41 @@
 #!/usr/bin/env bash
-declare -A counts
 
-main() {
-  local IFS=", "
-  for word in $1; do
-    counts[$word]=$((${counts[$word]}+1))
+#declare -A counts
+
+#main() {
+#  local IFS="${IFS}," 
+#  for word in $1; do
+#    counts[$word]=$((${counts[$word]}+1))
+#  done
+#  for i in ${!counts[*]}; do
+#    echo $i: ${counts[$i]}
+#  done
+#}
+
+#main "$@"
+         
+
+#=================================================================
+
+remove_quotes() {
+  [[ "$1" == \'*\' ]] && echo "${1:1:-1}" || echo "$1"
+}
+word_count() {
+  local words="${1,,}"
+  local cleaned
+  cleaned="${words//\\n/ }"
+  cleaned="${cleaned//[[:space:]]/ }"
+  cleaned="${cleaned//[^[:alnum:]\']/ }"
+  local -A -i counts
+  
+  for w in $cleaned; do
+    w=$(remove_quotes "$w")
+    ((counts['"$w"'] += 1)) # second expansion suppressed because of apostrophe
   done
-  for i in ${!counts[*]}; do
-    echo $i: ${counts[$i]}
+
+  for k in "${!counts[@]}"; do
+    echo "$k: ${counts[$k]}"
   done
 }
 
-
-
-main "$@"
+word_count "$@"

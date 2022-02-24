5ec468f22927e4117eb336ace155a2a0fe2a4304
AustinLat <alaturno@gmail.com>
2022-02-23 21:18:46 -0800

    still working on go time package

--- exercism/go/booking-up-for-beauty/booking_up_for_beauty.go
+++ exercism/go/booking-up-for-beauty/booking_up_for_beauty.go
@@ -1,6 +1,9 @@
 package booking
 
-import "time"
+import (
+  "time"
+  "fmt"
+)
 
 // Schedule returns a time.Time from a string containing a date
 func Schedule(date string) time.Time {
@@ -18,17 +21,18 @@ func HasPassed(date string) bool {
 // IsAfternoonAppointment returns whether a time is in the afternoon
 func IsAfternoonAppointment(date string) bool {
   appointment, _ := time.Parse("Monday, January 2, 2006 15:04:05", date)
-  noon, _ := time.Parse("15:04:05", "11:59:59")
-  closing, _ := time.Parse("15:04:05", "18:00:00")
-  return appointment.Before(closing) && appointment.After(noon)
+  return appointment.Hour() < 18 && appointment.Hour() >= 12
 }
 
 // Description returns a formatted string of the appointment time
 func Description(date string) string {
-	panic("Please implement the Description function")
+  appointment, _ := time.Parse("1/2/2006 15:04:05", date)
+  wd := appointment.Weekday()
+  aptTime, _ := fmt.Printf("You have an appointment on %s, July 25, 2019, at 13:45.", wd.String())
+  return aptTime
 }
 
 // AnniversaryDate returns a Time with this year's anniversary
 func AnniversaryDate() time.Time {
-	panic("Please implement the AnniversaryDate function")
+  panic("Please implement the AnniversaryDate function")
 }

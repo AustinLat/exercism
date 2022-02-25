178daddd8680584b93c376c7ac37a615706fe5de
AustinLat <alaturno@gmail.com>
2022-02-24 19:46:08 -0800

    solved time package issue and refactored solution

--- exercism/go/booking-up-for-beauty/booking_up_for_beauty.go
+++ exercism/go/booking-up-for-beauty/booking_up_for_beauty.go
@@ -2,37 +2,42 @@ package booking
 
 import (
   "time"
-  "fmt"
 )
 
 // Schedule returns a time.Time from a string containing a date
 func Schedule(date string) time.Time {
-  time, _ := time.Parse("1/2/2006 15:04:05", date)
-    return time
+  formats := []string{
+    "1/2/2006 15:04:05",
+    "January 2, 2006 15:04:05",
+    "Monday, January 2, 2006 15:04:05",
+    "2006-01-02",
+  }
+  for _ , fmat := range formats {
+    t, err := time.Parse(fmat, date)
+    if err == nil {
+      return t
+    }
+  }
+  panic("Unknown date format.")
 }
 
 // HasPassed returns whether a date has passed
 func HasPassed(date string) bool {
-  t, _ := time.Parse("January 2, 2006 15:04:05", date)
-  now := time.Now()
-  return t.Before(now)
+  return Schedule(date).Before(time.Now())
 }
 
 // IsAfternoonAppointment returns whether a time is in the afternoon
 func IsAfternoonAppointment(date string) bool {
-  appointment, _ := time.Parse("Monday, January 2, 2006 15:04:05", date)
-  return appointment.Hour() < 18 && appointment.Hour() >= 12
+  return Schedule(date).Hour() < 18 && Schedule(date).Hour() >= 12
 }
 
 // Description returns a formatted string of the appointment time
 func Description(date string) string {
-  appointment, _ := time.Parse("1/2/2006 15:04:05", date)
-  wd := appointment.Weekday()
-  aptTime, _ := fmt.Printf("You have an appointment on %s, July 25, 2019, at 13:45.", wd.String())
-  return aptTime
+  return Schedule(date).Format("You have an appointment on Monday, January 2, 2006, at 15:04.")
 }
 
 // AnniversaryDate returns a Time with this year's anniversary
 func AnniversaryDate() time.Time {
-  panic("Please implement the AnniversaryDate function")
+  return Schedule("2022-09-15")
 }
+

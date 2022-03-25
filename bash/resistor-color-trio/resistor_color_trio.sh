b525a9f82a764161338bfa43b3e0b209c61783c0
Austin Laturno <alaturno@gmail.com>
2022-03-25 00:49:04 +0000

    need to impliment zero counting for second color being zero

--- bash/resistor-color-trio/resistor_color_trio.sh
+++ bash/resistor-color-trio/resistor_color_trio.sh
@@ -14,20 +14,42 @@ main() {
   colors[grey]="8"
   colors[white]="9"
 
+  # Returning 0 ohms
+  [[ $1 == "black" ]] && [[ $2 == "black" ]] && echo 0 ohms && exit 0
+
+  # Testing for invalid input
+  for i in "$@"; do
+    if [[ ! ${colors[$i]+_} ]]; then
+      echo invalid color && exit 1
+    fi
+  done
+
   ohms=""
   count=0
-  while (( $count < 2 )); do
+  [[ $1 == "black" ]] && count=1 && shift
+  while (( count < 2 )); do
     ohms="${ohms}${colors[$1]}"
     let count=$count+1
     shift
+#    (( count == 1 )) && [[ $1 == "black" ]] && shift && break
   done
-  zeros=""
-  for i in $(seq ${colors[$1]}); do
-    zeros="${zeros}0"
-  done
-  echo ${zeros}
-  ohms="${ohms}${zeros}"
-  (( ${#zeros} > 2 )) && echo "${ohms} kiloohms" || echo "${ohms} ohms"
-}
 
+#  zeros=""
+#  for i in $(seq ${colors[$1]}); do
+#    zeros="${zeros}0"
+#  done
+
+  [[ $1 == "black" ]] && echo "${ohms} ohms" && exit 0
+  [[ $1 == "brown" ]] && echo "${ohms}0 ohms" && exit 0
+  [[ $1 == "red" ]] && echo "${ohms}00 ohms" && exit 0
+  [[ $1 == "orange" ]] && echo "${ohms} kiloohms" && exit 0
+  [[ $1 == "yellow" ]] && echo "${ohms}0 kiloohms" && exit 0
+  [[ $1 == "green" ]] && echo "${ohms}00 kiloohms" && exit 0
+  [[ $1 == "blue" ]] && echo "${ohms} megaohms" && exit 0
+  [[ $1 == "violet" ]] && echo "${ohms}0 megaohms" && exit 0
+  [[ $1 == "grey" ]] && echo "${ohms}00 megaohms" && exit 0
+  [[ $1 == "white" ]] && echo "${ohms} gigaohms" && exit 0
+
+  
+}
 main "$@"

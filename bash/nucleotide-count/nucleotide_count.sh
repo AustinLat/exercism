ad442ec2033d0c6490e8e4b2a358685ad3c698f3
Austin Laturno <alaturno@gmail.com>
2022-06-04 10:47:53 -0700

    working on bash arrays

--- bash/nucleotide-count/nucleotide_count.sh
+++ bash/nucleotide-count/nucleotide_count.sh
@@ -2,11 +2,22 @@
 
 main () {
   declare -A nucs
+  declare -A counts
   nucs=(["A"]=0 ["C"]=0 ["G"]=0 ["T"]=0)
   for i in $(seq 0 1 $((${#1}-1))); do
-    echo ${nucs[${1:$i:1}]}
+    if [[ -v nucs[${1:$i:1}] ]]; then
+      if [[ -v counts[${1:$i:1}] ]]; then
+        counts[${1:$i:1}]=$(( counts[${1:$i:1}]+1 ))
+      else
+        counts[${1:$i:1}]=1
+      fi
+    else
+      echo "Invalid nucleotide in strand" && exit 0
+    fi
   done
-#  echo ${nucs["C"]}
+  for i in $(seq 0 1 $((${#1}-1))); do
+    echo "${1:$i:1}: ${counts[${1:$i:1}]}" 
+  done
 }
 
 main "$@"

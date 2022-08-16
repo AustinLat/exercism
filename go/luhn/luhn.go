81289ee55ade31a12c2a7f668a4019beadfbe6ab
Austin Laturno <alaturno@gmail.com>
2022-08-15 21:09:40 -0700

    working on luhn formula in go

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -11,14 +11,18 @@ func Valid(id string) bool {
   if len(id) % 2 != 0 {
     startIndex = 1
   }
-  for i := startIndex; i < len(id); i++ {
+  for i := startIndex; i < len(id); i=i+2 {
     if id[i] * 2 > 9 {
       total += int(id[i])*2-9
     } else { total += int(id[i])*2 }
-  i++
+  }
+  if startIndex == 0 {
+    total = total + int(id[len(id)-1])
   }
   if total % 10 == 0 {
     return true
   }
   return false
 }
+
+

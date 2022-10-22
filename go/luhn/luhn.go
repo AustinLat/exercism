1ab108a75ed92fe30d946e09132eb8c62537e219
Austin Laturno <alaturno@gmail.com>
2022-10-21 21:32:57 -0700

    working on luhn in go

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -6,23 +6,39 @@ import (
 
 func Valid(id string) bool {
   id = strings.ReplaceAll(id, " ", "")
+  if len(id) <= 1 {
+    return false
+  }
+
   startIndex := 0
   total := 0
   if len(id) % 2 != 0 {
     startIndex = 1
   }
-  for i := startIndex; i < len(id); i=i+2 {
-    if id[i] * 2 > 9 {
-      total += int(id[i])*2-9
-    } else { total += int(id[i])*2 }
-  }
+
+  total += Doubles(startIndex, id)  
+ 
   if startIndex == 0 {
-    total = total + int(id[len(id)-1])
+    startIndex = 1
+  } else { startIndex = 0 }
+  
+  for i := startIndex; i < len(id); i=i+2 {
+    total += int(id[i])
   }
+
   if total % 10 == 0 {
     return true
   }
   return false
 }
 
+func Doubles(start int, input string) int {
+  tot := 0
+  for i := start; i < len(input); i=i+2 {
+    if input[i] * 2 > 9 {
+      tot = tot - 9
+    } else { tot += int(input[i])*2 }
+  }
+  return tot
+}
 

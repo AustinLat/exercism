a1046ebd9532e07f1737dd06e9d8ab774c3c0b98
austinlat <alaturno@gmail.com>
2026-02-25 17:29:36 -0800

    working on it

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -4,11 +4,17 @@ def equilateral(sides):
 	else:
 		return False
 	
-
-
 def isosceles(sides):
-    pass
+	equilateral(sides)
+	if sides[0] == sides[1] or sides[1] == sides[2] or sides [0] == sides[2]:
+		return True
+	else:
+		return False
 
 
 def scalene(sides):
-    pass
+	if sides[0] != sides[1] or sides[1] != sides[2] or sides [0] != sides[2]:
+		return True
+	else:
+		return False
+	

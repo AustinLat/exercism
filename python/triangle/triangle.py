973ed9fa7d6549cd6d4437d3c46a6cf7228acdc1
austinlat <alaturno@gmail.com>
2026-02-26 17:06:41 -0800

    almost solved

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -8,9 +8,16 @@ def equilateral(sides):
 		return False
 	
 def isosceles(sides):
-	for i in sides:
-		if i == 0:
+	for i, item in enumerate(sides):
+		if item == 0:
 			return False
+		if i == 0 and item > sides[1] + sides[2]:
+			return False
+		if i == 1 and item > sides[0] + sides[2]:
+			return False
+		if i == 2 and item > sides[0] + sides[1]:
+			return False
+		
 	if equilateral(sides) == True:
 		return True
 	if sides[0] == sides[1] or sides[1] == sides[2] or sides [0] == sides[2]:
@@ -20,9 +27,16 @@ def isosceles(sides):
 
 
 def scalene(sides):
-	for i in sides:
-		if i == 0:
+	for i, item in enumerate(sides):
+		if item == 0:
 			return False
+		if i == 0 and item > sides[1] + sides[2]:
+			return False
+		if i == 1 and item > sides[0] + sides[2]:
+			return False
+		if i == 2 and item > sides[0] + sides[1]:
+			return False
+
 	if sides[0] != sides[1] or sides[1] != sides[2] or sides [0] != sides[2]:
 		return True
 	else:
